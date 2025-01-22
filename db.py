41d278f42bf2292ce0a85c7442b620a6e9fd2cc9
SherhanK <timofey.kuznetsov2008@gmail.com>
2025-01-23 01:16:24 +0300

    Update db.py

--- db.py
+++ db.py
@@ -14,15 +14,24 @@ sql_create = '''CREATE TABLE users(
     stars INT,
     teacher TEXT);'''
 
-sql_create2 = '''CREATE TABLE tasks(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    right_answer TEXT NOT NULL,
-    status TEXT NOT NULL
+sql_create2 = '''CREATE TABLE tasks_status(
+    id_test INTEGER PRIMARY KEY AUTOINCREMENT,
+    student_id INTEGER REFERENCES users(id),
+    id_task INTEGER,
+    result TEXT,
+    bin_code TEXT,
+    date DATE
+);'''
+
+sql_create3 = '''CREATE TABLE student_tasks(
+    id_student INTEGER REFERENCES users(id),
+    id_test INTEGER REFERENCES tasks_status(id_test),
+    id_task INTEGER
 );'''
 
 cursor_db.execute(sql_create)
 cursor_db.execute(sql_create2)
+cursor_db.execute(sql_create3)
 
 db_lp.commit()
 db_lp.close()
