c9d847da271d2ed675803a8b64df18ba8077964e
SherhanK <timofey.kuznetsov2008@gmail.com>
2025-01-24 17:43:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sqlite3
 import json
 import codecs
+import bson
+import subprocess
+from datetime import date
 def auth(file):
     with open(file, encoding='utf-8') as f:
         data = json.load(f)
@@ -57,9 +60,24 @@ def star_add(file):
         data = json.load(f)
     email = data['email']
     stars_n = data['stars']
+    task_num = data['task_num']
     conn = sqlite3.connect('ItPying_users.db')
     cursor = conn.cursor()
     cursor.execute("UPDATE users SET stars = stars + ? WHERE email = ?", (stars_n, email))
+    cursor.execute("SELECT id FROM users WHERE email = ?", (email,))
+    user_id = cursor.fetchone()[0]
+    today = date.today()
+    cursor.execute("INSERT INTO tasks_status (student_id, id_task, result, date) VALUES (?, ?, ?, ?)", (user_id, task_num, '100/100', today.strftime("%d.%m.%Y")))
+    cursor.execute("SELECT id_test FROM tasks_status WHERE student_id = ? ORDER BY id_test DESC LIMIT 1", (user_id,))
+    test_num = cursor.fetchone()[0]
+    cursor.execute("SELECT id_test FROM student_tasks WHERE id_student = ? AND id_task = ?", (user_id, task_num))
+    if not cursor.fetchone():
+        cursor.execute("INSERT INTO student_tasks (id_student, id_test, id_task) VALUES (?, ?, ?)", (user_id, test_num, task_num))
+    else:
+        cursor.execute("SELECT id_test FROM student_tasks WHERE id_student = ? AND id_task = ?", (user_id, task_num))
+        last_test = cursor.fetchone()[0]
+        now_test = f"{last_test}/{test_num}"
+        cursor.execute("UPDATE student_tasks SET id_test =  ? WHERE id_student = ? AND id_task = ?", (now_test, user_id, task_num))
     conn.commit()
     conn.close()
     return True
@@ -98,15 +116,3 @@ def check_stars_class(file):
         with open('user_info.json', 'w', encoding='utf-8') as json_file:
             json.dump(user_info, json_file, ensure_ascii=False, indent=4)
         return user_info
-
-
-
-# a = {
-#     "email": "ivan@ivanov.ru",
-#     "stars": 5,
-#     "test_num": 1
-# }
-
-# with open('star.json', 'w', encoding='utf-8') as json_file:
-#     json.dump(a, json_file, ensure_ascii=False, indent=4)
-add_user('auth.json')
